902a42aaf47e7998a5478e11c4955ee2a1f17ea9
RandyMangar <RandyMangar@users.noreply.github.com>
2018-04-11 17:51:30 +0200

    Replaced the word 'Of' in every function with 'From' for consistancy

--- SeleniumAPI/Location.cs
+++ SeleniumAPI/Location.cs
@@ -1,4 +1,9 @@
-﻿using OpenQA.Selenium.Remote;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
 
 namespace SeleniumAPI
 {
@@ -12,32 +17,80 @@ namespace SeleniumAPI
             _remoteWebDriver = remoteWebDriver;
         }
 
-        //GetLocationOfElement
+        //GetLocationFromElement
+        public Point GetLocationFromElement(IWebElement webElement)
+        {
+            try
+            {
+                return webElement.Location;
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+                throw;
+            }
+        }
 
+        //GetLocationFromElements
+        public List<Point> GetLocationsFromElement(List<IWebElement> webElements)
+        {
+            try
+            {
+                var locationList = new List<Point>();
+                foreach (var webElement in webElements)
+                {
+                    locationList.Add(webElement.Location);
+                }
 
-        //GetLocationOfElements
-
+                return locationList;
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+                throw;
+            }
+        }
 
 
         //ClassName
 
 
+        //ListClassName
+
+
         //CssSelector
+
+
+        //ListCssSelector
 
 
         //Id
 
 
+        //ListId
+
         //LinkText
+
+
+        //ListLinkText
 
 
         //Name
 
 
+        //ListName
+
+
         //TagName
 
 
+        //ListTagName
+
+
         //XPath
+
+
+        //ListXPath
 
     }
 }
--- SeleniumAPI/Selected.cs
+++ SeleniumAPI/Selected.cs
@@ -12,10 +12,10 @@ namespace SeleniumAPI
             _remoteWebDriver = remoteWebDriver;
         }
 
-        //GetSelectedOfElement
+        //GetSelectedFromElement
 
 
-        //GetSelectedOfElements
+        //GetSelectedFromElements
 
 
 
